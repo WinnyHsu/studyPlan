8d1a55d60962b70a3cfe1dc16bd5119dee97774a
Winny Hsu <winny21019@gmail.com>
2026-06-05 00:13:44 +0800

    adjust cell

--- studyPlan/src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import AppShell from "@/components/layout/AppShell";
-import DashboardPage from "./dashboard/page";
-export default function Home() {
-  return <AppShell><DashboardPage /></AppShell>;
-}
